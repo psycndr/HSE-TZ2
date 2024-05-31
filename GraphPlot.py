854cd2b92b98d433f4f2ae99e51bffd824940e7b
psycndr <nice.day.lev@gmail.com>
2024-05-31 23:27:33 +0300

    update

---
+++ GraphPlot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('src/test/output/results.csv')
+
+sizes = data['Size'].to_numpy()
+times_sum = data['TimeSum'].to_numpy()
+
+plt.figure(figsize=(14, 8))
+
+plt.plot(sizes, times_sum, label='Time for _sum', marker='o')
+
+plt.xlabel('Size of input data')
+plt.ylabel('Execution time (ms)')
+plt.title('Execution time vs Size of input data')
+plt.legend()
+plt.grid(True)
+plt.xscale('log')
+plt.yscale('log')
+
+plt.xticks(sizes)
+
+plt.show()
